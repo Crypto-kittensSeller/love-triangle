b95ba4b8ba7e6b04e7363b4fd9737ef9e4be13d1
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2018-09-16 17:33:02 +0300

    Add triangle counter

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  var counttriangle = 0;
+  for (var i=0; i<preferences.length; i++)
+    if (preferences[preferences[preferences[i]-1]-1] == i+1)   {
+      if (preferences[i]!==i+1){
+        counttriangle++;
+        preferences[preferences[preferences[i]-1]-1] = 0;}}
+ 
+  return(counttriangle);
+  }
+
